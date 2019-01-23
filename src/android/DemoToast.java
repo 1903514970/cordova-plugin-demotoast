a9043160ec2ec45d133bdb6387c38db03a784407
dengjun <dengjun@sobey.com>
2019-01-23 16:49:11 +0800

    初次提交

---
+++ src/android/DemoToast.java
@@ -0,0 +1,48 @@
+package org.apache.cordova;
+
+import android.widget.Toast;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by dengjun on 2019/1/23.
+ */
+
+public class DemoToast extends CordovaPlugin {
+
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        final String message;
+        if(args != null){
+            message = args.getString(0);
+        }else{
+            message = null;
+        }
+        if(message == null){
+            callbackContext.error("toast message is null");
+            return true;
+        }
+        if("toastLong".equals(action)){
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(cordova.getContext(),message,Toast.LENGTH_LONG).show();
+                }
+            });
+        }else if("toast".equals(action)){
+            cordova.getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(cordova.getContext(),message,Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            return false;
+        }
+        return true;
+    }
+}
